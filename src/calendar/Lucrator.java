ec28d8c9fd0ff8b9b63681001ee13cae8fb9a42e
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-12-16 18:04:10 +0200

    Update Lucrator.java

--- src/calendar/Lucrator.java
+++ src/calendar/Lucrator.java
@@ -10,34 +10,19 @@ public class Lucrator {
     }
 
     public void lucreaza(String numeZi) throws ExceptieZiNelucratoare {
-        // abordare semigrupa 2
-        int indexZi = this.calendar.existaZi(numeZi);
-        if(indexZi == -1) {
-            System.out.println(numeZi + " NU este o zi a saptamanii");
-        } else {
-            if(this.calendar.getZile()[indexZi].esteLucratoare())   {
-                System.out.println("Lucratorul " + nume + " lucreaza " + numeZi);
-            }
-            else {
-                throw new ExceptieZiNelucratoare("Exceptie: lucru in zi nelucratoare");
-            }
-        }
-
-        // abordare semigrupa 1
-        /*
-        boolean gasita = false;
+        boolean esteGasita = false;
         for (int i = 0; i < this.calendar.getZile().length; i++) {
             Zi zi1 = this.calendar.getZile()[i];
             if (zi1.getNume().equals(numeZi)) {
-                gasita = true;
+                esteGasita = true;
                 if (zi1.esteLucratoare())
                     System.out.println("Lucratorul " + nume + " lucreaza " + numeZi);
                 else
                     throw new ExceptieZiNelucratoare("Exceptie: lucru in zi nelucratoare");
             }
         }
-        if (!gasita) {
+        if (!esteGasita) {
             System.out.println(numeZi + " NU este o zi a saptamanii");
-        }*/
+        }
     }
 }
