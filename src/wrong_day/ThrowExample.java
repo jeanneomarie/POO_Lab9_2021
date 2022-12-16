04403d5473a91aeae499d2fb6637e6c930d41679
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-12-16 18:01:19 +0200

    Update ThrowExample.java

--- src/wrong_day/ThrowExample.java
+++ src/wrong_day/ThrowExample.java
@@ -1,21 +1,39 @@
-package wrong_day;
+package wrongday;
 
 public class ThrowExample {
     public void doIt() throws WrongDayException {
         int dayOfWeek = (new java.util.Date()).getDay();
-        if (dayOfWeek != 2 && dayOfWeek != 4)
-            throw new WrongDayException("Tue. or Thur.");
+        if (dayOfWeek != 1 && dayOfWeek != 4)
+            throw new WrongDayException("Mon. or Thur.");
+        System.out.println("Did it");
+    }
+
+    public void doIt2() {
+        int dayOfWeek = (new java.util.Date()).getDay();
+        if (dayOfWeek != 1 && dayOfWeek != 4)
+            try {
+                throw new WrongDayException("Mon. or Thur.");
+            } catch (WrongDayException e) {
+                e.printStackTrace();
+                //return;
+            } finally {
+                System.out.println("finally got executed");
+            }
         System.out.println("Did it");
     }
 
     public static void main(String[] argv) {
-        try {
-            System.out.println("hello");
-            (new ThrowExample()).doIt();
-            System.out.println("bye");
-        } catch (WrongDayException e) {
-            System.out.println("Sorry, can do it only on " + e.getMessage());
-            e.printStackTrace();
-        }
+        (new ThrowExample()).doIt2();
+
+//        try {
+//            System.out.println("hello");
+//            (new ThrowExample()).doIt();
+//            System.out.println("bye");
+//        } catch (WrongDayException e) {
+//            System.out.println("Sorry, can do it only on " + e.getMessage());
+//        } finally {
+//            System.out.println("bye3");
+//        }
+//        System.out.println("bye2");
     }
 }
